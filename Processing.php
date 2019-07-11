6e65e7cf8a2ea44a07383f5769e563b01c29808c
Yiqun Chen <47789930+YiqunChen1999@users.noreply.github.com>
2019-07-11 11:28:13 +0800

    Update Processing.php

--- Processing.php
+++ Processing.php
@@ -1,10 +1,13 @@
+
+<!DOCTYPE html>
 <html>
-<header>
 <body>
-  
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+
+<?php
+$myfile = fopen("https://raw.githubusercontent.com/YiqunChen1999/YiqunChen1999.github.io/master/webdictionary.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("webdictionary.txt"));
+fclose($myfile);
+?>
 
 </body>
-</header>
 </html>
